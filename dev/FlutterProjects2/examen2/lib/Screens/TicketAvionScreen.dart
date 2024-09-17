cb30163e2fd93b496da2804b9fd74960adef071d
Elias Walton <elias_walton@hotmail.com>
2024-09-16 22:05:16 -0600

    agregue decoracion

--- dev/FlutterProjects2/examen2/lib/Screens/TicketAvionScreen.dart
+++ dev/FlutterProjects2/examen2/lib/Screens/TicketAvionScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 import 'package:examen2/Models/TicketAvion.dart';
 
 class TicketAvionScreen extends StatefulWidget {
@@ -23,23 +24,65 @@ class _TicketAvionScreenState extends State<TicketAvionScreen> {
         builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (!snapshot.hasData) return const CircularProgressIndicator();
           return ListView(
+            padding: const EdgeInsets.all(8.0),
             children: snapshot.data!.docs.map((document) {
               TicketAvion ticket = TicketAvion.fromMap(document.data() as Map<String, dynamic>, document.id);
-              return ListTile(
-                title: Text(ticket.nombre),
-                subtitle: Text('${ticket.destino} - ${ticket.fecha}'),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.edit),
-                      onPressed: () => _editTicket(ticket),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () => _deleteTicket(ticket.id),
-                    ),
-                  ],
+              String formattedDate = DateFormat('yyyy-MM-dd').format(ticket.fecha);
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8.0),
+                elevation: 4.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(16.0),
+                  title: Text(
+                    ticket.nombre,
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 8.0),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Destino: ',
+                          style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: ticket.destino,
+                              style: const TextStyle(fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Fecha: ',
+                          style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: formattedDate,
+                              style: const TextStyle(fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit, color: Colors.blue),
+                        onPressed: () => _editTicket(ticket),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () => _deleteTicket(ticket.id),
+                      ),
+                    ],
+                  ),
                 ),
               );
             }).toList(),
@@ -54,108 +97,122 @@ class _TicketAvionScreenState extends State<TicketAvionScreen> {
   }
 
   void _createTicket() {
-  showDialog(
-    context: context,
-    builder: (context) {
-      String nombre = '';
-      String destino = '';
-      DateTime fecha = DateTime.now();
+    showDialog(
+      context: context,
+      builder: (context) {
+        String nombre = '';
+        String destino = '';
+        DateTime fecha = DateTime.now();
 
-      return AlertDialog(
-        title: const Text('Crear Ticket'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              onChanged: (value) => nombre = value,
-              decoration: const InputDecoration(labelText: 'Nombre'),
-            ),
-            TextField(
-              onChanged: (value) => destino = value,
-              decoration: const InputDecoration(labelText: 'Destino'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                DateTime? pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: fecha,
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2101),
-                );
-                if (pickedDate != null && pickedDate != fecha) {
-                  setState(() {
-                    fecha = pickedDate;
-                  });
-                }
+        return AlertDialog(
+          title: const Text('Crear Ticket'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                onChanged: (value) => nombre = value,
+                decoration: const InputDecoration(labelText: 'Nombre'),
+              ),
+              TextField(
+                onChanged: (value) => destino = value,
+                decoration: const InputDecoration(labelText: 'Destino'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: fecha,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null && pickedDate != fecha) {
+                    setState(() {
+                      fecha = pickedDate;
+                    });
+                  }
+                },
+                child: const Text('Seleccionar Fecha'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                _firestore.collection('TicketAvion').add({
+                  'nombre': nombre,
+                  'destino': destino,
+                  'fecha': fecha,
+                });
+                Navigator.of(context).pop();
               },
-              child: const Text('Seleccionar Fecha'),
+              child: const Text('Guardar'),
             ),
           ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              _firestore.collection('TicketAvion').add({
-                'nombre': nombre,
-                'destino': destino,
-                'fecha': fecha,
-              });
-              Navigator.of(context).pop();
-            },
-            child: const Text('Guardar'),
-          ),
-        ],
-      );
-    },
-  );
-}
-
-
+        );
+      },
+    );
+  }
 
   void _editTicket(TicketAvion ticket) {
-  showDialog(
-    context: context,
-    builder: (context) {
-      String nombre = ticket.nombre;
-      String destino = ticket.destino;
-      DateTime fecha = ticket.fecha;
+    showDialog(
+      context: context,
+      builder: (context) {
+        String nombre = ticket.nombre;
+        String destino = ticket.destino;
+        DateTime fecha = ticket.fecha;
 
-      return AlertDialog(
-        title: const Text('Editar Ticket'),
-        content: Column(
-          children: [
-            TextField(
-              onChanged: (value) => nombre = value,
-              decoration: const InputDecoration(labelText: 'Nombre'),
-              controller: TextEditingController(text: nombre),
-            ),
-            TextField(
-              onChanged: (value) => destino = value,
-              decoration: const InputDecoration(labelText: 'Destino'),
-              controller: TextEditingController(text: destino),
-            ),
-            // Agregar un selector de fecha aquí
-          ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              _firestore.collection('TicketAvion').doc(ticket.id).update({
-                'nombre': nombre,
-                'destino': destino,
-                'fecha': fecha,
-              });
-              Navigator.of(context).pop();
-            },
-            child: const Text('Guardar'),
+        return AlertDialog(
+          title: const Text('Editar Ticket'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                onChanged: (value) => nombre = value,
+                decoration: const InputDecoration(labelText: 'Nombre'),
+                controller: TextEditingController(text: nombre),
+              ),
+              TextField(
+                onChanged: (value) => destino = value,
+                decoration: const InputDecoration(labelText: 'Destino'),
+                controller: TextEditingController(text: destino),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: fecha,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null && pickedDate != fecha) {
+                    setState(() {
+                      fecha = pickedDate;
+                    });
+                  }
+                },
+                child: const Text('Seleccionar Fecha'),
+              ),
+            ],
           ),
-        ],
-      );
-    },
-  );
-}
-
+          actions: [
+            TextButton(
+              onPressed: () {
+                _firestore.collection('TicketAvion').doc(ticket.id).update({
+                  'nombre': nombre,
+                  'destino': destino,
+                  'fecha': fecha,
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text('Guardar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   void _deleteTicket(String id) {
     _firestore.collection('TicketAvion').doc(id).delete();
